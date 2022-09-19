4d3328e6c779a91f1dc8432f918b868840ae8823
Long Dao <long.dh@teko.vn>
2022-09-19 22:59:18 +0700

    add code

--- src/robots.js
+++ src/robots.js
@@ -28,5 +28,35 @@ export const robots = [
         name: 'Ha Lan',
         username: 'Ngu Si',
         email: 'lanngusi@teko.vn'
+    },
+    {
+        id: 600,
+        name: 'Long Dao',
+        username: 'Hai Long',
+        email: 'long.dh@teko.vn'
+    },
+    {
+        id: 700,
+        name: 'Gia Bao',
+        username: 'Nghe',
+        email: 'bao.dg@teko.vn'
+    },
+    {
+        id: 800,
+        name: 'John',
+        username: 'Doe',
+        email: 'johndoe@teko.vn'
+    },
+    {
+        id: 900,
+        name: 'Phuong',
+        username: 'Pich',
+        email: 'pichphuong@teko.vn'
+    },
+    {
+        id: 1000,
+        name: 'Ha Lan',
+        username: 'Ngu Si',
+        email: 'lanngusi@teko.vn'
     }
 ]
