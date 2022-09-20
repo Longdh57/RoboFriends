c84a0eb3c1e317ee918b177d2b479a65c4163434
Long Dao <long.dh@teko.vn>
2022-09-20 10:41:51 +0700

    Refactor folder and create Scroll components

---
+++ src/containers/App.js
@@ -0,0 +1,54 @@
+import React, {Component} from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import './App.css';
+import Scroll from "../components/Scroll";
+
+class App extends  Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    async componentDidMount() {
+        await this.sleep(500)
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+            return response.json()
+        }).then(users => {
+            this.setState({robots: users})
+        })
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+    }
+
+    render() {
+        const {robots, searchField} = this.state
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
+        })
+        return !robots.length ?
+        (
+            <div className={'tc'}>
+                <h1 className={'f1'}>Loading...</h1>
+            </div>
+        ):
+        (
+            <div className={'tc'}>
+                <h1 className={'f1'}>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        )
+    }
+}
+
+export default App;
